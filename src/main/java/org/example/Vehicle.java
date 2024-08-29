3c86442b57666ab79ccc70e42002d6e81b731fa3
maradC <maradiagachristopher5@gmail.com>
2024-08-29 10:48:55 -0400

    Created default constructor and overloaded constructor

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -6,6 +6,20 @@ public class Vehicle {
     private float engineSize;
     private String fuelType;
 
+    Vehicle(){
+
+    }
+    Vehicle(int numberOfWheels, String color, float engineSize, String fuelType) {
+        this.numberOfWheels = numberOfWheels;
+        this.color = color;
+        this.engineSize = engineSize;
+        this.fuelType = fuelType;
+
+
+
+
+    }
+
 
 
 }
