4f6b1cd94a9d22818f520dbeb4bb6eb662354f8d
maradC <maradiagachristopher5@gmail.com>
2024-08-29 10:50:49 -0400

    Created getters and setters for Vehicle

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -14,10 +14,30 @@ public class Vehicle {
         this.color = color;
         this.engineSize = engineSize;
         this.fuelType = fuelType;
-
-
-
-
+    }
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public float getEngineSize() {
+        return engineSize;
+    }
+    public void setEngineSize(float engineSize) {
+        this.engineSize = engineSize;
+    }
+    public String getFuelType() {
+        return fuelType;
+    }
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
     }
 
 
