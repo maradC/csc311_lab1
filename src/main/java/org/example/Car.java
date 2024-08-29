d3f28c6b28a5e045f6ff27a0a1216474af3f810b
maradC <maradiagachristopher5@gmail.com>
2024-08-29 12:30:25 -0400

    Had to change access modifiers for Vehicle class in order to use super in order for display to work properly

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Car extends Vehicle {
+    private String brand;
+
+
+    public Car(String brand, int numberOfWheels, String color, float engineSize, String fuelType) {
+
+            super(numberOfWheels, color, engineSize, fuelType);
+            this.brand = brand;
+
+        }
+
+
+    public void honk() {
+        System.out.println("Honk, honk!");
+
+    }
+
+    public void displayInfo() {
+        System.out.println("Num of wheels: " + numberOfWheels + "\nColor: " + color + "\nEngine size: " + engineSize + "\nFuel type: " + fuelType + "\nBrand: " + brand);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+}
--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class Vehicle {
-    private int numberOfWheels;
-    private String color;
-    private float engineSize;
-    private String fuelType;
+    protected int numberOfWheels;
+    protected String color;
+    protected float engineSize;
+    protected String fuelType;
 
     Vehicle(){
 
